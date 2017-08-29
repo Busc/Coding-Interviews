93574f454d902a02182c46ea7af369f16bb01c80
Busc <762521772@qq.com>
2017-08-29 19:22:28 +0800

    数组

---
+++ 数据结构/03 二维数组中的查找.py
@@ -0,0 +1,22 @@
+class Solution:
+    # array 二维列表
+    def Find(self, target, array):
+        # write code here
+        for i in range(len(array)):
+            if target in array[i]:
+                return True
+        return False
+
+    def FindGenaral(self, target, array):
+        rowNum = len(array)
+        colNum = len(array[0])
+        row = 0
+        col =colNum - 1
+        while row<rowNum and col>=0:
+            if target == array[row][col]:
+                return True
+            elif target > array[row][col]:
+                row += 1
+            else:
+                col -= 1
+        return False
