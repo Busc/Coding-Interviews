84f39e9addce141f35c9b307aac06da38b34a7cb
Busc <762521772@qq.com>
2017-09-15 13:45:34 +0800

    二分查找，我做起来竟然相当费劲啊啊啊

---
+++ 01 算法和数据操作/08 旋转数组的最小数字.py
@@ -0,0 +1,29 @@
+# -*- coding:utf-8 -*-
+class Solution:
+    def minNumberInRotateArray(self, rotateArray):
+        # write code here
+        left, right = 0, len(rotateArray)-1
+        # 考虑到数组本身就是有序的情况
+        mid = 0
+        while rotateArray[left] >= rotateArray[right]:
+            if right - left == 1:
+                mid = right
+                break
+            mid = (right + left) // 2
+            if rotateArray[left] == rotateArray[mid] and rotateArray[right] == rotateArray[mid]:
+                # 无法判断最小值在哪一段
+                return self.orderSearch(rotateArray, left, right)
+            if rotateArray[mid] >= rotateArray[left]:
+                # 最小值位于后半段
+                left = mid
+            elif rotateArray[mid] <= rotateArray[right]:
+                # 最小值位于前半段或者就是mid
+                right = mid
+        return rotateArray[mid]
+
+    def orderSearch(self, rotateArray, firstInd, lastInd):
+        minNum = rotateArray[firstInd]
+        for i in range(firstInd+1, lastInd+1):
+            if minNum > rotateArray[i]:
+                minNum = rotateArray[i]
+        return minNum
