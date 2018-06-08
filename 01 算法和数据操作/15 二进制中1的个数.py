528e74535d42e3eb3548d9ec0536f5bc50df58e8
Busc <busc829@foxmail.com>
2018-06-09 00:34:43 +0800

    位运算

--- 01 算法和数据操作/15 二进制中1的个数.py
+++ 01 算法和数据操作/15 二进制中1的个数.py
@@ -1,5 +1,11 @@
 class Solution:
     def NumberOf1(self, n):
         # write code here
-        return sum([(n >> i & 1) for i in range(32)])
+        # return sum([(n >> i & 1) for i in range(32)])
         # 负数：补码 = 反码 + 1
+        # 把一个整数减去1后再和原来的整数做 位与 运算，得到的结果相当于把整数的二进制表示中 最右边的1变成0
+        count = 0
+        while n:
+            n = (n-1) & n
+            count += 1
+        return count
